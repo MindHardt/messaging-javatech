a23ca3d02468f0f0144a4bdddc22bff4de99e482
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-12-10 22:55:25 +0500

    Done ✅

--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,5 +1,6 @@
 package com.example.messagingrabbitmq;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -19,9 +20,14 @@ public class Runner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			System.out.println("Write message: ");
+			String str = in.nextLine();
+
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "foo.bar.baz", str);
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
 	}
 
 }
